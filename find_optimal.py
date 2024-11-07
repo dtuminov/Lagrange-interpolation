981d789bec4116457bad6a670bbfaa5948aa25dc
Дмитрий Туминов <d.tuminov1@outlook.com>
2024-11-08 00:21:24 +0300

    find optimal - function which finds opt value of n for given finction

---
+++ find_optimal.py
@@ -0,0 +1,29 @@
+import numpy as np
+from lagrange_interpolation import lagrange_interpolation
+
+
+def find_optimal_n(func, n_values, num_samples=1001):
+    optimal_n = None
+    min_max_error = float('inf')
+
+    for n in n_values:
+        # Генерируем узлы интерполяции
+        nodes = np.linspace(0, 2, n)
+        # Вычисляем значения функции в узлах
+        y_nodes = func(nodes)
+
+        # Создаем более плотную выборку для вычисления интерполяции
+        x_fine = np.linspace(0, 2, num_samples)
+
+        # Вычисляем интерполированные значения
+        y_interpolated = lagrange_interpolation(x_fine, nodes, y_nodes)
+
+        # Вычисляем максимальное отклонение от оригинальной функции
+        max_error = np.max(np.abs(y_interpolated - func(x_fine)))
+
+        # Проверяем, является ли текущее отклонение минимальным
+        if max_error < min_max_error:
+            min_max_error = max_error
+            optimal_n = n
+
+    return optimal_n, min_max_error
